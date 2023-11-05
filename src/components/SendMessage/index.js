ac1444c53c6482f0cc1629b39d0da6edf653a2d4
Mösyö Coder <128539219+mosyocoder@users.noreply.github.com>
2023-11-05 19:01:48 +0300

    Update index.js

--- src/components/SendMessage/index.js
+++ src/components/SendMessage/index.js
@@ -4,30 +4,29 @@ import axios from "axios";
 function SendMessage() {
 	const [message, setMessage] = useState("");
 
-	const sendMessage = async () => {
-		
-			const data = {
-				message: message,
-				user_key: localStorage.getItem("anonymous_chat_app"),
+
+			const sendMessage = async () => {
+	const data = {
+		message: message,
+		user_key: localStorage.getItem("anonymous_chat_app"),
+	}
+	try {
+		await axios.post(
+			`https://${process.env.REACT_APP_HASURA_LINK}api/rest/addmessage`,
+			{
+				data,
+			},
+			{
+				headers: {
+					"x-hasura-admin-secret": process.env.REACT_APP_X_HASURA_ADMIN_SECRET,
+				},
 			}
-			try {
-				await axios.post(
-					`https://${process.env.REACT_APP_HASURA_LINK}api/rest/addmessage`,
-					{
-						data,
-					},
-					{
-						headers: {
-							"x-hasura-admin-secret": process.env.REACT_APP_X_HASURA_ADMIN_SECRET,
-						},
-					},
-				);
-				setMessage("");
-			} catch (err) {
-				console.log("Error :", err.message);
-			}
-		}
-	};
+		);
+		setMessage("");
+	} catch (err) {
+		console.log("Error :", err.message);
+	}
+};
 
 	return (
 		<div className="inputArea">
